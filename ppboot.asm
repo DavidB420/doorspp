34a8410cd6e5d66ac94aada82afab1598160766e
DavidB420 <davidbadiei123@gmail.com>
2023-04-29 15:20:45 -0400

    UEFI bootloader

---
+++ ppboot.asm
@@ -0,0 +1,34 @@
+format pe64 dll efi
+entry main
+section '.text' code executable readable
+main:
+
+sub rsp,64
+
+mov r8,Variables
+mov [r8],rcx
+mov [r8+8],rdx
+
+mov rcx,[rdx+64]
+lea rdx,[greetingString]
+call qword [rcx+8]
+
+call WaitForKeyPress
+
+add rsp,64
+
+ret
+
+WaitForKeyPress:
+;mov rcx,[
+ret
+
+section '.data' data readable writeable
+
+greetingString du 'Hello World',0
+
+Variables:
+EfiHandle dq 0
+EfiTable dq 0
+
+section '.reloc' fixups data discardable
