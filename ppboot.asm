a0c17749300682d6ac50e58e685de2a1ed758fa9
David Badiei <37677391+DavidB420@users.noreply.github.com>
2023-12-24 00:13:19 -0500

    Got bootloader working on real metal

--- ppboot.asm
+++ ppboot.asm
@@ -21,7 +21,9 @@ call setupGOP
 ;Exit boot services
 mov rax,qword [efiSystemTable]
 mov rax,[rax+EFI_SYSTEM_TABLE.BootServices]
+sub rsp,32
 call qword [rax+EFI_BOOT_SERVICES_TABLE.ExitBootServices]
+add rsp,32
 ;Move file to proper memory address
 mov rsi,[efiOSBufferHandle]
 mov rdi,0x20000
@@ -68,7 +70,9 @@ mov rdx,0
 lea r8,[efiGOPHandle]
 mov r9,qword [efiSystemTable]
 mov r9,[r9+EFI_SYSTEM_TABLE.BootServices]
+sub rsp,32
 call qword [r9+EFI_BOOT_SERVICES_TABLE.LocateProtocol]
+add rsp,32
 ;Get current video mode
 mov rcx,[efiGOPHandle]
 mov edx,dword [rcx+EFI_GRAPHICS_OUTPUT_PROTOCOL.Mode]
@@ -76,7 +80,9 @@ mov edx,dword [rdx+EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE.CurrentMode]
 and edx,edx
 lea r8,[gopModeSize]
 lea r9,[gopModeInfo]
+sub rsp,32
 call qword [rcx+EFI_GRAPHICS_OUTPUT_PROTOCOL.QueryMode]
+add rsp,32
 mov r9, qword [gopModeInfo]
 mov eax,dword [r9+EFI_GRAPHICS_OUTPUT_MODE_INFORMATION.VerticalResolution]
 mov ebx,dword [r9+EFI_GRAPHICS_OUTPUT_MODE_INFORMATION.HorizontalResolution]
@@ -92,7 +98,9 @@ mov rdx,rcx
 mov rcx,[efiGOPHandle]
 lea r8,[gopModeSize]
 lea r9,[gopModeInfo]
+sub rsp,32
 call qword [rcx+EFI_GRAPHICS_OUTPUT_PROTOCOL.QueryMode]
+add rsp,32
 mov r9, qword [gopModeInfo]
 mov eax,dword [r9+EFI_GRAPHICS_OUTPUT_MODE_INFORMATION.VerticalResolution]
 mov ebx,dword [r9+EFI_GRAPHICS_OUTPUT_MODE_INFORMATION.HorizontalResolution]
@@ -107,7 +115,9 @@ loop loopgetgopmodes
 ;Set the highest resolution as the video mode
 mov dl,byte [currentHighestIndex]
 mov rcx,[efiGOPHandle]
+sub rsp,32
 call qword [rcx+EFI_GRAPHICS_OUTPUT_PROTOCOL.SetMode]
+add rsp,32
 ;Save video framebuffer and size
 mov rax,[efiGOPHandle]
 mov rcx,[rax+EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE.FrameBufferBase]
@@ -123,7 +133,9 @@ mov rdx,EFI_LOADED_IMAGE_PROTOCOL_GUID
 lea r8,[efiLoadedImage]
 mov r9,qword [efiSystemTable]
 mov r9,[r9+EFI_SYSTEM_TABLE.BootServices]
+sub rsp,32
 call qword [r9+EFI_BOOT_SERVICES_TABLE.HandleProtocol]
+add rsp,32
 ;Get volume handle
 mov rcx,[efiLoadedImage]
 mov rcx,[rcx+EFI_LOADED_IMAGE_PROTOCOL.DeviceHandle]
@@ -131,11 +143,15 @@ mov rdx,EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID
 lea r8,[efiVolumeHandle]
 mov r9,qword [efiSystemTable]
 mov r9,[r9+EFI_SYSTEM_TABLE.BootServices]
+sub rsp,32
 call qword [r9+EFI_BOOT_SERVICES_TABLE.HandleProtocol]
+add rsp,32
 ;Open Volume
 mov rcx,[efiVolumeHandle]
 lea rdx,[efiRootFSHandle]
+sub rsp,32
 call qword [rcx+EFI_SIMPLE_FILE_SYSTEM_PROTOCOL.OpenVolume]
+add rsp,32
 ret
 
 openFile:
@@ -143,21 +159,26 @@ openFile:
 mov rcx,[efiRootFSHandle]
 lea rdx,[efiFileHandle]
 mov r9,EFI_FILE_MODE_READ
+sub rsp,32
 call qword [rcx+EFI_FILE_PROTOCOL.Open]
+add rsp,32
 cmp rax,0
 je skiperrorloadingkernel
+sub rsp,8
 mov rsi,errorStr
 call printString
+add rsp,8
 call waitForAnyKey
-mov al,0xfe
-out 0x64,al
+call resetPC
 skiperrorloadingkernel:
 ;Get file size
 mov rcx,[efiFileHandle]
 mov rdx,EFI_FILE_INFO_ID_GUID
 lea r8,[efiFileInfoBufferSize]
 mov r9,efiFileInfoBuffer
+sub rsp,40
 call qword [rcx+EFI_FILE_PROTOCOL.GetInfo]
+add rsp,40
 mov r9,[efiFileInfoBuffer+8]
 mov qword [efiReadSize],r9
 ;Allocate memory pool
@@ -166,29 +187,51 @@ mov rdx,qword [efiReadSize]
 lea r8,[efiOSBufferHandle]
 mov r9,qword [efiSystemTable]
 mov r9,[r9+EFI_SYSTEM_TABLE.BootServices]
+sub rsp,32
 call qword [r9+EFI_BOOT_SERVICES_TABLE.AllocatePool]
+add rsp,32
 ;Read file
 mov rcx,[efiFileHandle]
 lea rdx,[efiReadSize]
 mov r8,[efiOSBufferHandle]
+sub rsp,32
 call qword [rcx+EFI_FILE_PROTOCOL.Read]
+add rsp,32
 ret
 
 waitForAnyKey:
 mov rdx,1
 mov rcx,[efiSystemTable]
 mov rcx,[rcx+EFI_SYSTEM_TABLE.ConIn]
+sub rsp,32
 call qword [rcx+EFI_SIMPLE_TEXT_INPUT_PROTOCOL.Reset]
+add rsp,32
 loopwaitforkeypress:
 lea rdx,[efiKeyData]
 mov rcx,[efiSystemTable]
 mov rcx,[rcx+EFI_SYSTEM_TABLE.ConIn]
+sub rsp,32
 call qword [rcx+EFI_SIMPLE_TEXT_INPUT_PROTOCOL.ReadKeyStroke]
+add rsp,32
 and rax,0xff
 cmp rax,6 ;Check if its not ready
 je loopwaitforkeypress
 ret
 
+resetPC:
+;Reset using 8042 method (if possible)
+mov al,0xfe
+out 0x64,al
+;Reset using UEFI runtime services
+mov rax,qword [efiSystemTable]
+mov rax,[rax+EFI_SYSTEM_TABLE.RuntimeServices]
+mov rax,[rax+EFI_RUNTIME_SERVICES_TABLE.ResetSystem]
+mov r9d,0
+mov r8d,0
+mov edx,0
+mov ecx,0
+call rax
+ret
 
 printString:
 push rdx
